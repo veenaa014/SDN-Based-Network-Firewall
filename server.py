f2bd0ce8a6f6319b94aa3fe998d62f421e4c045a
veenaa014 <47256853+veenaa014@users.noreply.github.com>
2021-02-07 18:39:30 -0700

    Server paths to support frontend

--- server.py
+++ server.py
@@ -1,26 +1,54 @@
 from flask import Flask
-import threading
+ from flask import request, send_from_directory, redirect, url_for
+ import threading
 
-import pymongo
-from pymongo import MongoClient
+ import pymongo
+ @@ -9,18 +10,45 @@
+
+ client = MongoClient('3.92.107.201', 27017)
+ logdb = client.pox.log
+ rules = client.pox.rules
+
+ @app.route('/')
+ def index():
+     col = client.pox.rules
+     return "Hello, World!" + str(col.find_one()['protocol'])
+     return "Hello, World!"
+
+ @app.route('/gui/<path:path>')
+ def gui(path):
+     return send_from_directory('frontend', path)
+
+ @app.route('/log')
+ def getlog():
+     res = ""
+     for entry in logdb.find(limit=40).sort("_id", pymongo.DESCENDING):
+         res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "] " + entry['message'] + "\n"
+         res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "];" + entry['message'] + "\n"
+     return res
+
+ @app.route('/rules', methods=['GET'])
+ def getrules():
+     res = ""
+     for entry in rules.find():
+         res = res + entry['protocol'] + ";" + entry['source'] + ";" + entry['destination'] + ";" + str(entry['_id']) + ","
+     return res
+
+ @app.route('/deleterules')
+ def delrules():
+     source = request.args.get('source')
+     des = request.args.get('destination')
+     protocol = request.args.get('protocol')
+     rules.delete_many({"source": source, "destination": des, "protocol": protocol.lower()})
+     return redirect('http://localhost/gui/index2.html')
+
+ @app.route("/testform")
+ def do_post_search():
+     source = request.args.get('source')
+     des = request.args.get('destination')
+     protocol = request.args.get('protocol')
+     rules.insert_one({"source": source, "destination": des, "protocol": protocol.lower()})
+     return redirect('http://localhost/gui/index2.html')
 
 
-app = Flask(__name__)
-
-client = MongoClient('3.92.107.201', 27017)
-logdb = client.pox.log
-
-@app.route('/')
-def index():
-    col = client.pox.rules
-    return "Hello, World!" + str(col.find_one()['protocol'])
-
-@app.route('/log')
-def getlog():
-    res = ""
-    for entry in logdb.find(limit=40).sort("_id", pymongo.DESCENDING):
-        res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "] " + entry['message'] + "\n"
-    return res
-
-
-app.run(debug=False, port=80, host='0.0.0.0')
+ app.run(debug=False, port=80, host='0.0.0.0') 
