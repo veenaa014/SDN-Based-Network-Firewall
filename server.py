990b71e5df2da4718985fdb48163b26e50b89676
veenaa014 <47256853+veenaa014@users.noreply.github.com>
2021-02-07 18:43:09 -0700

    server paths to support frontend

--- server.py
+++ server.py
@@ -1,54 +1,54 @@
 from flask import Flask
- from flask import request, send_from_directory, redirect, url_for
- import threading
+from flask import request, send_from_directory, redirect, url_for
+import threading
 
- import pymongo
- @@ -9,18 +10,45 @@
-
- client = MongoClient('3.92.107.201', 27017)
- logdb = client.pox.log
- rules = client.pox.rules
-
- @app.route('/')
- def index():
-     col = client.pox.rules
-     return "Hello, World!" + str(col.find_one()['protocol'])
-     return "Hello, World!"
-
- @app.route('/gui/<path:path>')
- def gui(path):
-     return send_from_directory('frontend', path)
-
- @app.route('/log')
- def getlog():
-     res = ""
-     for entry in logdb.find(limit=40).sort("_id", pymongo.DESCENDING):
-         res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "] " + entry['message'] + "\n"
-         res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "];" + entry['message'] + "\n"
-     return res
-
- @app.route('/rules', methods=['GET'])
- def getrules():
-     res = ""
-     for entry in rules.find():
-         res = res + entry['protocol'] + ";" + entry['source'] + ";" + entry['destination'] + ";" + str(entry['_id']) + ","
-     return res
-
- @app.route('/deleterules')
- def delrules():
-     source = request.args.get('source')
-     des = request.args.get('destination')
-     protocol = request.args.get('protocol')
-     rules.delete_many({"source": source, "destination": des, "protocol": protocol.lower()})
-     return redirect('http://localhost/gui/index2.html')
-
- @app.route("/testform")
- def do_post_search():
-     source = request.args.get('source')
-     des = request.args.get('destination')
-     protocol = request.args.get('protocol')
-     rules.insert_one({"source": source, "destination": des, "protocol": protocol.lower()})
-     return redirect('http://localhost/gui/index2.html')
+import pymongo
+from pymongo import MongoClient
 
 
- app.run(debug=False, port=80, host='0.0.0.0') 
+app = Flask(__name__)
+
+client = MongoClient('3.92.107.201', 27017)
+logdb = client.pox.log
+rules = client.pox.rules
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+@app.route('/gui/<path:path>')
+def gui(path):
+    return send_from_directory('frontend', path)
+
+@app.route('/log')
+def getlog():
+    res = ""
+    for entry in logdb.find(limit=40).sort("_id", pymongo.DESCENDING):
+        res = res + "[" + entry['time'].strftime("%m/%d/%Y, %H:%M:%S.%f") + "];" + entry['message'] + "\n"
+    return res
+
+@app.route('/rules', methods=['GET'])
+def getrules():
+    res = ""
+    for entry in rules.find():
+        res = res + entry['protocol'] + ";" + entry['source'] + ";" + entry['destination'] + ";" + str(entry['_id']) + ","
+    return res
+
+@app.route('/deleterules')
+def delrules():
+    source = request.args.get('source')
+    des = request.args.get('destination')
+    protocol = request.args.get('protocol')
+    rules.delete_many({"source": source, "destination": des, "protocol": protocol.lower()})
+    return redirect('http://localhost/gui/index2.html')
+
+@app.route("/testform")
+def do_post_search():
+    source = request.args.get('source')
+    des = request.args.get('destination')
+    protocol = request.args.get('protocol')
+    rules.insert_one({"source": source, "destination": des, "protocol": protocol.lower()})
+    return redirect('http://localhost/gui/index2.html')
+
+
+app.run(debug=False, port=80, host='0.0.0.0')
